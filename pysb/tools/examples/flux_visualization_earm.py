2873ddce14bf5382e44caf99443ef37288c51cb4
oscar <oo.ortega2247@gmail.com>
2016-07-13 16:23:54 -0500

    deleted and old script and changed example to use earm1.0

--- pysb/tools/examples/flux_visualization_earm.py
+++ pysb/tools/examples/flux_visualization_earm.py
@@ -1,17 +1,9 @@
 from __future__ import print_function
-from earm.lopez_embedded import model
+from pysb.examples.earm_1_0 import model
 from pysb.tools.model_visualization import run_visualization
 import numpy as np
-import csv
-
-# type Bax cluster: 5400
-# type Bak cluster: 4052
-
-f = open('/home/oscar/Documents/tropical_project/parameters_5000/pars_embedded_4052.txt')
-data = csv.reader(f)
-parames = [float(i[1]) for i in data]
 
 tspan = np.linspace(0, 20160, 100)
 
-run_visualization(model, tspan, parameters=parames, render_type='flux', verbose=False)
+run_visualization(model, tspan, render_type='flux', save_video=True)
 print('finished')
