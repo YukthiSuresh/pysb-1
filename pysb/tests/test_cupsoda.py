a0d883022d7447d73c0480ae78eaabb25e56e103
Alex Lubbock <code@alexlubbock.com>
2016-09-23 16:08:33 -0500

    Unit tests and standardized cupsoda run signature
    Note that cupsoda unit tests are not run on travis due to lack of GPU/CUDA
    infrastructure. Developers should run the tests locally before committing
    to github.

--- pysb/tests/test_cupsoda.py
+++ pysb/tests/test_cupsoda.py
@@ -1,103 +1,39 @@
-import warnings
-import numpy as np
-from nose.plugins.attrib import attr
 from pysb.examples.tyson_oscillator import model
-from pysb.simulator.cupsoda import CupSodaSimulator
+from pysb.cupsoda import CupsodaSolver
+import numpy as np
+import warnings
+from nose.plugins.attrib import attr
 
 
 @attr('gpu')
 def test_cupsoda_tyson():
-    tspan = np.linspace(0, 500, 101)
+    tspan = np.linspace(0, 500, 501)
 
-    solver = CupSodaSimulator(model, tspan=tspan, atol=1e-12, rtol=1e-12,
+    solver = CupsodaSolver(model, tspan=tspan, atol=1e-12, rtol=1e-6,
                            max_steps=20000, verbose=False)
-    # tests of size 3 seem to fail on smaller gpus
-    n_sims = 50
-    vol = 1e-19
+
+    n_sims = 3
 
     # Rate constants
-    len_parameters = len(model.parameters)
-    param_values = np.ones((n_sims, len_parameters))
-    for j in range(len_parameters):
-        param_values[:, j] *= model.parameters[j].value
+    param_values = np.ones((n_sims, len(model.parameters)))
+    for i in range(len(param_values)):
+        for j in range(len(param_values[i])):
+            param_values[i][j] *= model.parameters[j].value
 
     # Initial concentrations
-    len_model_species = len(model.species)
-    y0 = np.zeros((n_sims, len_model_species))
-
-    for ic in model.initial_conditions:
-        for j in range(len_model_species):
-            if str(ic[0]) == str(model.species[j]):
-                y0[:, j] = ic[1].value
-                break
+    y0 = np.zeros((n_sims, len(model.species)))
+    for i in range(len(y0)):
+        for ic in model.initial_conditions:
+            for j in range(len(y0[i])):
+                if str(ic[0]) == str(model.species[j]):
+                    y0[i][j] = ic[1].value
+                    break
 
     with warnings.catch_warnings():
         warnings.filterwarnings('ignore', "Neither 'y0' nor 'param_values' "
                                           "were supplied.")
-        solver.run(param_values=None, initials=None)
+        solver.run(param_values=None, y0=None)
 
-    simres = solver.run(y0=y0,
-                        gpu=0,
-                        max_steps=20000,
-                        obs_species_only=True,
-                        memory_usage='sharedconstant',
-                        vol=vol)
-    print(simres.observables)
+    solver.run(param_values=param_values, y0=None)
     solver.run(param_values=None, y0=y0)
     solver.run(param_values=param_values, y0=y0)
-
-
-@attr('gpu')
-def test_memory_cases():
-    tspan = np.linspace(0, 500, 101)
-
-    solver = CupSodaSimulator(model, tspan=tspan, atol=1e-12, rtol=1e-12,
-                           max_steps=20000, verbose=False)
-
-    n_sims = 50
-
-    # Initial concentrations
-    len_model_species = len(model.species)
-    y0 = np.zeros((n_sims, len_model_species))
-
-    for ic in model.initial_conditions:
-        for j in range(len_model_species):
-            if str(ic[0]) == str(model.species[j]):
-                y0[:, j] = ic[1].value
-                break
-
-    with warnings.catch_warnings():
-        warnings.filterwarnings('ignore', "Neither 'y0' nor 'param_values' "
-                                          "were supplied.")
-        solver.run(param_values=None, initials=None)
-
-    solver.run(y0=y0, gpu=0, memory_usage='global')
-    solver.run(y0=y0, gpu=0, memory_usage='shared')
-    solver.run(y0=y0, gpu=0, memory_usage='sharedconstant')
-
-
-@attr('gpu')
-def test_use_of_volume():
-    tspan = np.linspace(0, 500, 101)
-
-    solver = CupSodaSimulator(model, tspan=tspan, atol=1e-12, rtol=1e-12,
-                           max_steps=20000, verbose=False)
-
-    n_sims = 50
-    vol = 1e-19
-
-    # Initial concentrations
-    len_model_species = len(model.species)
-    y0 = np.zeros((n_sims, len_model_species))
-
-    for ic in model.initial_conditions:
-        for j in range(len_model_species):
-            if str(ic[0]) == str(model.species[j]):
-                y0[:, j] = ic[1].value
-                break
-
-    solver.run(initials=y0, gpu=0, memory_usage='sharedconstant', outdir='.',
-               vol=vol)
-
-
-test_cupsoda_tyson()
