b13aad779b25ba11afdd9e5ad84ff5b797991030
oscar <oo.ortega2247@gmail.com>
2016-10-15 16:51:01 -0500

    change option to save video to false

--- pysb/tools/examples/flux_visualization_earm.py
+++ pysb/tools/examples/flux_visualization_earm.py
@@ -1,8 +1,9 @@
 from __future__ import print_function
-from pysb.examples.earm_1_0 import model
+# from pysb.examples.earm_1_0 import model
+from earm.lopez_embedded import model
 from pysb.tools.model_visualization import run_visualization
 import numpy as np
 
 tspan = np.linspace(0, 20160, 100)
-run_visualization(model, tspan, render_type='flux', save_video=True)
+run_visualization(model, tspan, render_type='flux', save_video=False)
 
