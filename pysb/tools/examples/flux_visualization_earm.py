1555a9311f30e565f0401f86599dafa81f0289d4
oscar <oo.ortega2247@gmail.com>
2017-01-29 12:19:29 -0600

    update path to file

--- pysb/tools/examples/flux_visualization_earm.py
+++ pysb/tools/examples/flux_visualization_earm.py
@@ -10,9 +10,9 @@ import csv
 # type1 Bid cluster: 3905
 # type2 Bid cluster: 2415
 
-f = open('/home/oscar/home/oscar/Documents/tropical_project/parameters_5000/pars_embedded_2415.txt')
+f = open('/home/oscar/Documents/tropical_earm_different_rates/parameters_5000/pars_embedded_2415.txt')
 data = csv.reader(f)
 parames = [float(i[1]) for i in data]
 tspan = np.linspace(0, 20160, 100)
-run_visualization(model, tspan, parameters=parames, render_type='flux', save_video=False)
+run_visualization(model, tspan, parameters=parames, render_type='flux', save_video=False, verbose=True)
 
